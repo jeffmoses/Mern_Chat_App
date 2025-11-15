c94b8127125876fe84bb6a6e1e46710e0cd975df
jeffmoses <jeffmosesotiende@gmail.comgit config --global user,name jeffmosesgit commit -m first>
2025-11-15 03:22:14 +0300

    update

--- client/src/components/ChatWindow.jsx
+++ client/src/components/ChatWindow.jsx
@@ -8,6 +8,7 @@ const ChatWindow = ({ messages, currentUser, typingUsers, messagesEndRef }) => {
     return new Date(timestamp).toLocaleTimeString([], {
       hour: '2-digit',
       minute: '2-digit',
+      second: '2-digit'
     });
   };
 
